bb3d269edcfd9c60a6fdeff6ef7d495c9541a988
olliecomet <leyechi@gmail.com>
2019-09-26 16:32:37 -0700

    passing first studio route test --ollie

--- e2e/__tests__/studios.test.js
+++ e2e/__tests__/studios.test.js
@@ -2,7 +2,54 @@ const request = require('../request');
 const db = require('../db');
 
 describe('studios api', () => {
-    beforeEach(() => {
-      return db.dropCollection('studios');
+  beforeEach(() => {
+    return db.dropCollection('studios');
+  });
+
+  const studio = {
+    name: 'Studio A',
+    address: {
+      city: 'Portland',
+      state: 'OR',
+      country: 'US'
+    }
+  };
+
+  function postStudio(studio) {
+    return request
+      .post('/api/studios')
+      .send(studio)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('post a studio', () => {
+    return postStudio(studio).then(studio => {
+      expect(studio).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        ...studio
+      });
     });
+  });
+
+  // it('gets a studio by id', () => {
+    // return postStudio(studio).then(studio => {
+      // return request
+        // .post('/api/studios')
+        // .send({
+          // _id: expect.any(String),
+          // title: expect.any(String)
+        // })
+        // .expect(200)
+        // .then(() => {
+          // return request.get(`/api/studios/${studio._id}`).expect(200);
+        // })
+        // .then(({ body }) => {
+          // expect(body).toMatchInlineSnapshot(
+            // _
+          // )
+        // })
+    // })
+  // });
 });
