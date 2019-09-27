6ec9033afb270467e4f2c2facd1ec819bcebaa91
olliecomet <leyechi@gmail.com>
2019-09-27 14:08:00 -0700

    made actor post and get by id routes --ollie

--- e2e/__tests__/actors.test.js
+++ e2e/__tests__/actors.test.js
@@ -1,75 +1,64 @@
-// const request = require('../request');
-// const db = require('../db');
+const request = require('../request');
+const db = require('../db');
 
-// describe('actors api', () => {
-//   beforeEach(() => {
-//     return db.dropCollection('actors');
-//   });
+describe('actors api', () => {
+  beforeEach(() => {
+    return db.dropCollection('actors');
+  });
 
-//   const actor = {
-//     name: 'Jonny Karate',
-//     dob: new Date('January 1 1995'),
-//     pob: 'Oregon'
-//   };
+  const actor = {
+    name: 'Jonny Karate',
+    dob: new Date('January 1 1995'),
+    pob: 'Oregon'
+  };
 
-//   function postStudio(actor) {
-//     return request
-//       .post('/api/actors')
-//       .send(studio)
-//       .expect(200)
-//       .then(({ body }) => body);
-//   }
+  function postActor(actor) {
+    return request
+      .post('/api/actors')
+      .send(actor)
+      .expect(200)
+      .then(({ body }) => body);
+  }
 
-//   it('post a studio', () => {
-//     return postStudio(studio).then(studio => {
-//       expect(studio).toEqual({
-//         _id: expect.any(String),
-//         __v: 0,
-//         ...studio
-//       });
-//     });
-//   });
+  it('posts an actor', () => {
+    return postActor(actor).then(actor => {
+      expect(actor).toEqual({
+        _id: expect.any(String),
+        __v: 0,
+        ...actor
+      });
+    });
+  });
 
-//   it('gets a studio by id', () => {
-//     return postStudio(studio)
-//       .then(studio => {
-//         return request.get(`/api/actors/${studio._id}`)
-//           .expect(200)
-//           .then(({ body }) => {
-//             expect(body).toEqual(studio);
-//           });
-//       });
-//   });
+  it('gets an actor by id', () => {
+    return postActor(actor)
+      .then(actor => {
+        return request.get(`/api/actors/${actor._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual(actor);
+          });
+      });
+  });
 
-//   it('gets a list of actors', () => {
-//     const firstStudio = {
-//       name: 'First Studio',
-//       address: {
-//         city: 'Orlando',
-//         state: 'Florida',
-//         country: 'USA'
-//       }
-//     };
-//     return Promise.all([
-//       postStudio(firstStudio),
-//       postStudio({ name: 'Second Studio' }),
-//       postStudio({ name: 'Third Studio' })
-//     ])
-//       .then(() => {
-//         return request.get('/api/actors').expect(200);
-//       })
-//       .then(({ body }) => {
-//         expect(body.length).toBe(3);
-//         expect(body[0]).toEqual({
-//           _id: expect.any(String),
-//           __v: expect.any(Number),
-//           name: firstStudio.name,
-//           address: {
-//             city: firstStudio.address.city,
-//             state: firstStudio.address.state,
-//             country: firstStudio.address.country
-//           }
-//         });
-//       }); 
-//   });
-// });
+  // it('gets a list of actors', () => {
+  //   const firstActor = {
+  //     name: 'Jonny Karate',
+  //     dob: new Date('January 1 1995'),
+  //     pob: 'Oregon'
+  //   };
+
+  //   return Promise.all([
+  //     postActor(firstActor),
+  //     postActor({ name: 'second actor' }),
+  //     postActor({ name: 'third actor' })
+  //   ])
+  //     .then(() => {
+  //       return request.get('/api/actors')
+  //         .expect(200);
+  //     })
+  //     .then(({ body }) => {
+  //       expect(body.length).toBe(3);
+  //     });
+  // });
+});
