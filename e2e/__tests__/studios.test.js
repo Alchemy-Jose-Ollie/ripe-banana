12e2aeb9483799e77fe31a686c8ffd50f6c54f4c
olliecomet <leyechi@gmail.com>
2019-09-26 17:00:07 -0700

    Jose - wrote test for get by id route

--- e2e/__tests__/studios.test.js
+++ e2e/__tests__/studios.test.js
@@ -33,23 +33,40 @@ describe('studios api', () => {
     });
   });
 
-  // it('gets a studio by id', () => {
-    // return postStudio(studio).then(studio => {
-      // return request
-        // .post('/api/studios')
-        // .send({
-          // _id: expect.any(String),
-          // title: expect.any(String)
-        // })
-        // .expect(200)
-        // .then(() => {
-          // return request.get(`/api/studios/${studio._id}`).expect(200);
-        // })
-        // .then(({ body }) => {
-          // expect(body).toMatchInlineSnapshot(
-            // _
-          // )
-        // })
-    // })
-  // });
+  it('gets a studio by id', () => {
+    return postStudio(studio)
+      .then(studio => {
+        return request.get(`/api/studios/${studio._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual(studio);
+          });
+      });
+  });
+
+  it('gets a list of studios', () => {
+    const firstStudio = {
+      name: 'First Studio',
+      address: {
+        city: 'Orlando',
+        state: 'Florida',
+        country: 'USA'
+      }
+    };
+    return Promise.all([
+      postStudio(firstStudio),
+      postStudio({ name: 'Second Studio' }),
+      postStudio({ name: 'Third Studio' })
+    ])
+      .then(() => {
+        return request.get('/api/studios').expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(body[0]).toEqual({
+          _id: expect.any(String),
+          name: firstStudio.name
+        });
+      }); 
+  });
 });
