7ec831f0b7636b834193043778ffccfb437424b8
olliecomet <leyechi@gmail.com>
2019-09-27 16:03:57 -0700

    Jose-- wrote review model started testing

---
+++ e2e/__tests__/reviews.test.js
@@ -0,0 +1,21 @@
+// const request = require('../request');
+// const db = require('../db');
+
+// describe('review api', () => {
+//   beforeEach(() => {
+//     return Promise.all([
+//       db.dropCollection('reviews'),
+//       db.dropCollection('reviewers'),
+//       db.dropCollection('films')
+//     ]);
+//   });
+
+//   const reviewer = 
+
+//   const review = {
+//     rating: 4,
+//     reviewer: {},
+//     review: 'This movie was kind of amazing',
+//     film: new ObjectId(),
+//   }
+// });
